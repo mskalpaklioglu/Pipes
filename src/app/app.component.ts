c623c4fcd70d15de6a5fc0cc662e4732c95e6dde
Muhammet Şükrü Kalpaklıoğlu <77413367+mskalpaklioglu@users.noreply.github.com>
2023-08-21 04:31:45 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  name = '';
+  date = '';
+  money :number=0;
+  height:number =0;
+  miles:number=0;
+
+  //lessons={
+    //name:'Math',
+    //subject:'NaturalNumber',
+    //currentPoint: 65
+  //}
+
+  onMilesChange(value: string)
+  {
+    this.miles=parseFloat(value);
+  }
+
+
+  onNameChange(value: string)
+  {
+    this.name =value;
+  }
+
+  onDateChange(value: string)
+  {
+    this.date=value;
+  }
+
+
+  onMoneyChange(value:string)
+  {
+    this.money=parseFloat(value);
+  }
+
+  onHeightChange(value:string)
+  {
+    this.height=parseFloat(value);
+  }
 }
+
+
